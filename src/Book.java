8cb8b4c8c91daf09b522618041a2d3274055c839
Valstion <lordvalstion@gmail.com>
2023-06-01 16:17:10 +0300

    Geterai Seterai

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Book {
+public class Book {
+    private String title;
+    private int pages;
+    private int releaseYear;
+    public Book() {
+    }
+    public Book(String title, int pages, int releaseYear) {
+        this.title = title;
+        this.pages = pages;
+        this.releaseYear = releaseYear;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public int getPages() {
+        return pages;
+    }
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Book[] books = new Book[6];
+        Book book1 = new Book();
+        Book book2 = new Book("Pavadinimas 1", 100, 2000);
+        Book book3 = new Book();
+        book3.setTitle("Pavadinimas 2");
+        book3.setPages(200);
+        book3.setReleaseYear(2005);
+        books[0] = book1;
+        books[1] = book2;
+        books[2] = book3;
+        for (Book book : books) {
+            if (book != null) {
+                System.out.println("Title: " + book.getTitle());
+                System.out.println("Pages: " + book.getPages());
+                System.out.println("Release Year: " + book.getReleaseYear());
+                System.out.println();
+            }
+        }
     }
 }
